04a676a3364163c09f0316db483cb9c20800790c
Suzanne K. Wallace <suzywallace501@gmail.com>
2019-03-15 22:50:39 +0100

    Added plan for notebook tests with NotebookScripter

---
+++ tests/test_Notebook.py
@@ -0,0 +1,5 @@
+'''
+Use notebook scripter to feed in dirs for dummy data with receive_param
+Use run_notebook function to return module.E_q_lat and compare to result for sample antisite, vacancy and interstitial
+After testing dummy data, rm .ipynb from this dir and run tests on main copy
+'''
