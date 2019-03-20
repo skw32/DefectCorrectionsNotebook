34216593562d3ba4c703b67424ba1686122edd6b
Suzanne K. Wallace <suzywallace501@gmail.com>
2019-03-20 11:49:59 +0100

    Testing re-adding coveralls badge

--- tests/test_CoffeeIntegration.py
+++ tests/test_CoffeeIntegration.py
@@ -1,32 +1,21 @@
-# For 'run_CoFFEE_solver'
-## Check that strings written to put into sub-process are what are expected
-### def check_subprocess_strings, assert command_for_subprocess == "correct string"
-
-# For 'write_CoFFEE_in_file' and 'run_CoFFEE_solver'
-## Write dummy 'in' coffee input files that is expected to run very quickly and compare to expected result 
-### def check_basic_solver, assert 
+import CoffeeConvenienceFunctions as ccf
+import re
+import os
+import pytest
 
 '''
-import re
-from ??/CoffeeConvenienceFunctions import * # Look into best way of loading functions from main dir
-
-
-
-def check_subprocess_strings:
-
-    assert command_for_subprocess == "correct string"
-
-
-
-
 def test_coffee_solver:
-    coffee_solver_output = 'coffee_solver_test.out'
     # Known energy output for test system
-    expectedOutput = 6.5
+    expectedEnergy = 0.4008
+    coffee_solver_output = 'coffee_solver_test.out'
+    # Parameters for known test system
+    sigma = 2.108966292
     # Run coffee Poission solver with test system (for quick convergence)
-    run_CoFFEE_solver(path_to_coffee_dir, defect_outputs_dir, super_x, super_y, super_z, defect_geom, sigma, cutoff, defect_charge, defect_x, defect_y, defect_z, dielectric_xx, dielectric_yy, dielectric_zz)
+    run_CoFFEE_solver(path_to_coffee_dir, defect_outputs_dir, 2, 2, 2, "tests/TestData/antisite/geometry.in", sigma, cutoff, 1, defect_x, defect_y, defect_z, dielectric_xx, dielectric_yy, dielectric_zz)
 
     # CHECK FOR NO ERROR MESSAGES!
+
+
     try:
         with open(coffee_solver_output), 'r') as f:
             for line in f:
@@ -38,6 +27,5 @@ def test_coffee_solver:
     except IOError:
         print("Could not open "+str(coffee_solver_output)) 
 
-    assert  E_q_per_m < expectedOutput+0.1 and E_q_per_m > expectedOutput+0.1
-
-'''
+    assert  E_q_per_m == pytest.approx(expectedEnergy)
+    '''
