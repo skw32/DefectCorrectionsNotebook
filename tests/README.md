fe5b9734aed1c565238e96b82aee45a4769b5b3b
Suzanne K. Wallace <suzywallace501@gmail.com>
2019-03-20 12:59:00 +0100

    Added test for implementing coffee poisson solver with test data and added extra check when reading in data from output file to notebook to account for e.g. cases where sigma value is too large

--- tests/README.md
+++ tests/README.md
@@ -1,9 +1,7 @@
-**To-do: hook up with Travis-CI or circle-CI??**
-
-Tests in this directory can be run with `pytest` from the top level directory of this repository.
+Tests in this directory can be run with `pytest` from the top level directory of this repository, or with `pytest --cov-config .coveragerc --cov=. tests/` for a test coverage information.
 
 The different test scripts include:
-- test_DataReadIn.py: this script contains tests for functions in 'DefectSupercellAnalyses.py' in the previous directory which check that data from FHI-aims structure files is being read in as expected using sample data contained in the 'TestData' directory.
+- test_DataReadIn.py: this script contains tests for functions in 'DefectSupercellAnalyses.py' in the previous directory which check that data from FHI-aims structure files is being read in as expected using sample data contained in the 'TestData' directory and also tests that the function used to configure the log file written to during the notebook workflow is being called without error.
 - test_CoffeeIntegration.py: this script contains tests for functions in 'CoffeeConvenienceFunctions.py' that automatically generate inputs for coffee.py and that instructions to run coffee.py are implemented correctly.
 - test_Notebook.py: this involves integration tests for multiple operations performed within the 'DefectCorrectionsNotebook.ipynb' for energy correction terms for sample defects. Sample data is inputted into the notebook and parameters (such as energy correction terms) are extracted using the NotebookScripter library.
 
--- tests/test_CoffeeIntegration.py
+++ tests/test_CoffeeIntegration.py
@@ -3,29 +3,35 @@ import re
 import os
 import pytest
 
-'''
-def test_coffee_solver:
-    # Known energy output for test system
+def test_coffee_solver():
+    # Known energy output for test system (2x2x2 supercell of antisite test defect supercell)
     expectedEnergy = 0.4008
-    coffee_solver_output = 'coffee_solver_test.out'
+    coffee_solver_output = 'cm_2x2x2.out'
+    defect_outputs_dir = 'ProcessedDefects'
+    os.makedirs(defect_outputs_dir, exist_ok=True)
+    coffee_exe = "coffee.py"
     # Parameters for known test system
+    super_x, super_y, super_z = 2, 2, 2
+    defect_geom = 'tests/TestData/antisite/geometry.in'
     sigma = 2.108966292
+    cutoff = 6.773799249378561
+    defect_charge = 1
+    defect_x, defect_y, defect_z = 5.58437198, 8.56614992, 6.21005598
+    dielectric_xx, dielectric_yy, dielectric_zz = 7.49, 6.92, 7.19
     # Run coffee Poission solver with test system (for quick convergence)
-    run_CoFFEE_solver(path_to_coffee_dir, defect_outputs_dir, 2, 2, 2, "tests/TestData/antisite/geometry.in", sigma, cutoff, 1, defect_x, defect_y, defect_z, dielectric_xx, dielectric_yy, dielectric_zz)
-
-    # CHECK FOR NO ERROR MESSAGES!
-
-
+    ccf.run_CoFFEE_solver(coffee_exe, defect_outputs_dir, super_x, super_y, super_z, defect_geom, sigma, cutoff, defect_charge, defect_x, defect_y, defect_z, dielectric_xx, dielectric_yy, dielectric_zz)
     try:
-        with open(coffee_solver_output), 'r') as f:
+        with open(os.path.join(defect_outputs_dir, coffee_solver_output), 'r') as f:
             for line in f:
                 if re.search('!', line):
                     words = line.split()
-                    E_q_per_m = float(words[4])
+                    if  words[1:4] == ['Total', 'Energy', '(eV):']:
+                        testEnergy = float(words[4])
                     if line == None:
-                        print('Warning! - Error finding E_q^{per,m} from '+str(coffee_solver_output))
+                        print('Warning! - Error finding E_q^{per,m} from '+str(os.path.join(defect_outputs_dir, coffee_solver_output)))
+                    else:
+                        print('Check '+str(os.path.join(defect_outputs_dir, coffee_solver_output))+' for error messages')
     except IOError:
-        print("Could not open "+str(coffee_solver_output)) 
+        print('Could not open '+str(os.path.join(defect_outputs_dir, coffee_solver_output))) 
 
-    assert  E_q_per_m == pytest.approx(expectedEnergy)
-    '''
+    assert  testEnergy == pytest.approx(expectedEnergy)
